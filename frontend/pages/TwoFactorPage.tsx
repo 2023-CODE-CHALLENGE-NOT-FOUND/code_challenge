adb428a6fd2252f9adb784504b769c0cc93f15d4
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2023-04-20 12:20:57 -0500

    [FIX][CS] Auth validation

--- frontend/pages/TwoFactorPage.tsx
+++ frontend/pages/TwoFactorPage.tsx
@@ -1,10 +1,14 @@
 import { getAuth, signOut } from "firebase/auth";
 
 export default function TwoFactorPage (){
-    const auth = getAuth();
-    const user = auth.currentUser
-    console.log("user", user)
-    console.log(auth)
+    try{
+        const auth = getAuth();
+        const user = auth.currentUser
+        console.log("user", user)
+        console.log(auth)
+    } catch (e) {
+        console.log(e)
+    }
     return (
         <>
             Hello
